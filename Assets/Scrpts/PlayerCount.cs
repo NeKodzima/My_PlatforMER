5cbd4e8c4b002f8b325d16e8bd1d4289cfd97da7
NeKodzima <68467695+NeKodzima@users.noreply.github.com>
2020-08-20 17:20:32 +0300

    добавлены ключи и шипы

--- Assets/Scrpts/PlayerCount.cs
+++ Assets/Scrpts/PlayerCount.cs
@@ -7,9 +7,12 @@ using UnityEngine.SceneManagement;
 public class PlayerCount : MonoBehaviour
 {
     public static int coins = 0;
+    public static int keys = 0;
     public static int OnStartCions = 0;
+    public static int OnStartKeys = 0;
     public static int hp = 2;
     public Text CoinCount;
+    public Text KeyCount;
     public Text HPCount;
     public Animator animator;
 
@@ -19,12 +22,14 @@ public class PlayerCount : MonoBehaviour
         animator.SetBool("IsDying", false);
         HPCount = GameObject.FindGameObjectWithTag("Hptext").GetComponent<Text>();
         CoinCount = GameObject.FindGameObjectWithTag("CoinText").GetComponent<Text>();
+        KeyCount = GameObject.FindGameObjectWithTag("KeyText").GetComponent<Text>();
     }
 
 
     void Update()
     {
         CoinCount.text=coins.ToString("0");
+        KeyCount.text=keys.ToString("0");
         HPCount.text=hp.ToString("0");
         if (hp <= 0)
         {
